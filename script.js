281adf29c16735531eb5e5fcc02f6a3cf9740aa7
[ghayastanoli] <[ghiyastanoli@yahoo.com]>
2024-12-20 02:38:29 +0500

    chore: UI Progress

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navContainer = document.querySelector('.nav-container');
+    const leftNavImage = document.querySelector('.left-nav img');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            navContainer.classList.remove('transparent');
+            navContainer.classList.add('scrolled');
+            leftNavImage.src = "https://www.felixhomes.com/images/logo_black.svg";
+        } else {
+            navContainer.classList.remove('scrolled');
+            navContainer.classList.add('transparent');
+            leftNavImage.src = "https://www.felixhomes.com/images/logo_white.svg";
+        }
+    });
+
+    // Initialize as transparent
+    navContainer.classList.add('transparent');
+});
